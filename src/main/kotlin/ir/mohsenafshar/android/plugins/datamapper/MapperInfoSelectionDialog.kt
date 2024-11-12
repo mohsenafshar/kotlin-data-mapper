62bc95ec3b2bf4363022c84d2702a20957e64532
mohsen afshar <ma.developer.ir@gmail.com>
2024-11-12 17:09:53 +0330

    Code clean up

--- src/main/kotlin/ir/mohsenafshar/android/plugins/datamapper/MapperInfoSelectionDialog.kt
+++ src/main/kotlin/ir/mohsenafshar/android/plugins/datamapper/MapperInfoSelectionDialog.kt
@@ -4,13 +4,8 @@ import com.intellij.ide.util.TreeClassChooserFactory
 import com.intellij.ide.util.TreeFileChooserFactory
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
@@ -32,9 +27,7 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
     }
     private val extensionFunctionRadio = JRadioButton("Extension Function", true)
     private val globalFunctionRadio = JRadioButton("Global Function")
-    private val generateSeparateFileCheckbox = JCheckBox("Generate in a separate file")
     private val fileNameField = JBTextField().apply { isEnabled = false }
-    private val fileNameLabel = JLabel("File Name:", SwingConstants.LEFT)
 
     init {
         title = "Generate Mapping Function"
@@ -69,8 +62,8 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
 
         // Function Type Selection Panel
         val functionTypePanel = JPanel().apply {
-//            val roundedLineBorder = LineBorder(JBColor(Color.LIGHT_GRAY, Color.DARK_GRAY), 2, true)
-            border = BorderFactory.createTitledBorder("Function Type").apply { titleColor = JBColor(Color.BLACK, Color.WHITE) }
+            border = BorderFactory.createTitledBorder("Function Type")
+                .apply { titleColor = JBColor(Color.BLACK, Color.WHITE) }
             add(extensionFunctionRadio)
             add(globalFunctionRadio)
         }
@@ -87,35 +80,8 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
         mainPanel.add(targetFilePanel)
         mainPanel.add(Box.createVerticalStrut(8))
         mainPanel.add(functionTypePanel)
-//        mainPanel.add(getGenerateInNewFileCheckboxPanel())
 
         return mainPanel
-    }
-
-    private fun getGenerateInNewFileCheckboxPanel(): JPanel {
-        val fileOptionPanel = JPanel(BorderLayout()).apply {
-            add(generateSeparateFileCheckbox, BorderLayout.NORTH)
-
-            val fileNamePanel = JPanel(BorderLayout()).apply {
-                add(fileNameLabel, BorderLayout.WEST)
-                fileNameLabel.isEnabled = false
-                add(fileNameField, BorderLayout.CENTER)
-                fileNameField.isEnabled = false // Initially hidden
-            }
-            add(fileNamePanel, BorderLayout.SOUTH)
-        }
-
-        generateSeparateFileCheckbox.addItemListener {
-            fileNameLabel.isEnabled = generateSeparateFileCheckbox.isSelected
-            fileNameField.isEnabled = generateSeparateFileCheckbox.isSelected
-            if (generateSeparateFileCheckbox.isSelected) {
-                fileNameField.text = "GeneratedMapper"
-            }
-
-            this.window.pack()  // Adjust dialog size
-        }
-
-        return fileOptionPanel
     }
 
     private fun createSelectClassButton(classField: JBTextField, dialogTitle: String): JButton {
@@ -126,11 +92,6 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
                 classChooser.showDialog()
                 val selectedClass = classChooser.selected
                 classField.text = selectedClass?.qualifiedName ?: ""
-
-                // Set default file name if checkbox is checked and source class is selected
-                if (generateSeparateFileCheckbox.isSelected && classField == sourceClassField) {
-                    fileNameField.text = selectedClass?.name ?: "GeneratedMapper"
-                }
             }
         }
     }
@@ -138,12 +99,6 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
     private fun createSelectFileButton(fileField: JBTextField, dialogTitle: String): JButton {
         return JButton("...").apply {
             addActionListener {
-//                val fileDesc = FileChooserDescriptor(true, false, false, false, false, false)
-//                FileChooser.chooseFile(fileDesc, project, null) { consumer: VirtualFile ->
-//                    println(consumer.name)
-//                    classField.text = consumer.name ?: ""
-//                }
-
                 val fileChooser = TreeFileChooserFactory.getInstance(project).createFileChooser(
                     dialogTitle, null,
                     KotlinFileType.INSTANCE, null,
@@ -165,32 +120,6 @@ class NewClassSelectionDialog(private val project: Project, event: AnActionEvent
 
     fun isExtensionFunctionSelected(): Boolean {
         return extensionFunctionRadio.isSelected
-    }
-
-    fun isSeparateFileGenerationEnabled(): Boolean {
-        return generateSeparateFileCheckbox.isSelected
-    }
-
-    fun getFileName(): String? {
-        return if (isSeparateFileGenerationEnabled()) fileNameField.text else null
-    }
-
-    // Function to get the Document for the selected source class
-    fun getSourceClassDocument(): Document? {
-        val sourceClassName = sourceClassField.text
-        if (sourceClassName.isNotEmpty()) {
-            // Find the PsiClass for the source class name
-            val psiClass =
-                JavaPsiFacade.getInstance(project).findClass(sourceClassName, GlobalSearchScope.projectScope(project))
-            val psiFile = psiClass?.containingFile
-
-            if (psiFile != null) {
-                // Get the VirtualFile and then the Document
-                val virtualFile: VirtualFile? = psiFile.virtualFile
-                return virtualFile?.let { FileDocumentManager.getInstance().getDocument(it) }
-            }
-        }
-        return null
     }
 }
 
